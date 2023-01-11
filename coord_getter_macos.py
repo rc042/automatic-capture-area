e44d2751e8a8b06e07fbaa0a433ceec124551606
Frédéric PENG <frederic.peng@pasapas.com>
2023-01-11 17:51:55 +0100

    Add script for MacOS coordonates 'coord_getter_macos.py'

--- coord_getter_macos.py
+++ coord_getter_macos.py
@@ -5,7 +5,9 @@ print('Press Ctrl-C to quit.')
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        x = x * 2
+        y = y * 2
+        positionStr = str(x).rjust(4) + ', ' +  str(y).rjust(4)
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
